f559cefa10e4de9b3f56c463a16b6f9e1de17530
iuryhenzo78@gmail.com <iuryhenzo78@gmail.com>
2023-06-07 18:17:39 +0000

    include tables transform

---
+++ models/staging/clean_bike_rides.sql
@@ -0,0 +1,18 @@
+SELECT
+    SUM(duration_minutes) as total_minutes
+    , COUNT(rental_id) as total_bike_hires
+    , ROUND(SUM(duration_minutes) / COUNT(rental_id), 2) AS average_duration
+    , EXTRACT(month from start_date) as month
+    , CASE
+        WHEN EXTRACT(HOUR from TIMESTAMP(start_date)) >= 6 AND EXTRACT(HOUR from TIMESTAMP(start_date)) <= 10 THEN 'Morning Peak'
+        WHEN EXTRACT(HOUR from TIMESTAMP(start_date)) >= 16 AND EXTRACT(HOUR from TIMESTAMP(start_date)) <= 19 THEN 'Evening Peak'
+        ELSE 'Off-Peak'
+      END AS start_peak_travel
+    , IF(start_station_id = end_station_id, True, False) as same_station_flag
+    , start_station_id
+    , start_station_name
+    , end_station_id
+    , end_station_name
+FROM {{ ref('raw_bike_hires') }}
+GROUP BY 4,5,6,7,8,9,10
+ORDER BY total_minutes DESC
---
+++ models/staging/raw_bike_stations.sql
@@ -0,0 +1,9 @@
+SELECT 
+    id
+    , name as station_name
+    , bikes_count
+    , docks_count
+    , install_date
+    , removal_date
+FROM `bigquery-public-data.london_bicycles.cycle_stations`
+WHERE install_date < '2017-01-01' and (removal_date < '2018-01-01' or removal_date is null)
