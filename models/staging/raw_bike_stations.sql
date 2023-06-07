b21544665f755ef5f2337cea3029f9fec55cd737
iuryhenzo78@gmail.com <iuryhenzo78@gmail.com>
2023-06-07 19:52:49 +0000

    change schema

--- models/staging/raw_bike_stations.sql
+++ models/staging/raw_bike_stations.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 SELECT 
     id
     , name as station_name
@@ -5,5 +7,5 @@ SELECT
     , docks_count
     , install_date
     , removal_date
-FROM `bigquery-public-data.london_bicycles.cycle_stations`
+FROM {{ source('london_bicycles', 'cycle_stations')}}
 WHERE install_date < '2017-01-01' and (removal_date < '2018-01-01' or removal_date is null)
